fa4b92d613dbc8f0a06ece7701a81a71ea298c83
Saatvik Arya <aryasaatvik@gmail.com>
2021-05-26 19:54:29 -0700

    Initial Commit

---
+++ berri/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  berri
+//
+//  Created by Saatvik Arya on 5/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ berri/berriApp.swift
@@ -0,0 +1,17 @@
+//
+//  berriApp.swift
+//  berri
+//
+//  Created by Saatvik Arya on 5/26/21.
+//
+
+import SwiftUI
+
+@main
+struct berriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
