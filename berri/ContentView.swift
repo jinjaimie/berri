e979e2056d40b9c7fc8be7f02b60048f57a20dab
Jaimie Jin <jinjaimie12@gmail.com>
2021-05-28 18:28:36 -0700

    add buttons

--- berri/ContentView.swift
+++ berri/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        SettingView()
+    }
+}
+
+struct SettingView: View {
+    var body: some View {
+        Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+            Text("Add an account")
+        }.padding()
+        Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+            Text("Add a category")
+        }.padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SettingView()
     }
 }
