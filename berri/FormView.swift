c29652ea3f76f9fb1f060aec3f61ead889f8fad2
Audrey Kho <ackho@uw.edu>
2021-06-01 03:13:15 -0700

    layouting done

--- berri/FormView.swift
+++ berri/FormView.swift
@@ -18,69 +18,71 @@ struct AccountForm: View {
     
     var body: some View {
         GeometryReader { geom in
-            VStack (spacing: 30) {
-                HStack {
-                    Text("New account").font(.largeTitle).fontWeight(.bold)
+            ScrollView {
+                VStack (spacing: 30) {
+                    HStack {
+                        Text("New account").font(.largeTitle).fontWeight(.bold)
+                        Spacer()
+                    }.padding(.bottom, geom.size.height*0.02)
+                    HStack {
+                        TextField("Enter Name", text: $name).padding().border(Color.gray).cornerRadius(8.0)
+                        Spacer()
+                    }
+                    HStack {
+                        TextField("Enter Initial Amount", text: $amount).padding().border(Color.gray).cornerRadius(8.0)
+                        Spacer()
+                    }
                     Spacer()
-                }.padding(.bottom, geom.size.height*0.02)
-                HStack {
-                    TextField("Enter Name", text: $name).padding().border(Color.gray).cornerRadius(8.0)
-                    Spacer()
-                }
-                HStack {
-                    TextField("Enter Initial Amount", text: $amount).padding().border(Color.gray).cornerRadius(8.0)
-                    Spacer()
-                }
-                Spacer()
-                HStack {
-                    Button(action: {
-                        let initial = Double(amount)
-                        self.ref.child("addAccount/\(name)/").getData { (error, snapshot) in
-                            if snapshot.exists() {
-                                showAlert = true
-                                alertType = "existingAcct"
-                            } else if (initial == nil) {
-                                showAlert = true
-                                alertType = "invalidAmount"
-                            } else if (error == nil) {
-                                self.ref.child("addAccount").child(name).setValue(["amount": initial])
-                            } else {
-                                print("Error")
+                    HStack {
+                        Button(action: {
+                            let initial = Double(amount)
+                            self.ref.child("addAccount/\(name)/").getData { (error, snapshot) in
+                                if snapshot.exists() {
+                                    showAlert = true
+                                    alertType = "existingAcct"
+                                } else if (initial == nil) {
+                                    showAlert = true
+                                    alertType = "invalidAmount"
+                                } else if (error == nil) {
+                                    self.ref.child("addAccount").child(name).setValue(["amount": initial])
+                                } else {
+                                    print("Error")
+                                }
+                            }
+                        }, label: {
+                            Text("Save")
+                        }).disabled(name.isEmpty || amount.isEmpty)
+                        .alert(isPresented: $showAlert) {
+                            switch alertType {
+                                case "existingAcct":
+                                    return Alert(
+                                        title: Text("Error"),
+                                        message: Text("This account already exists"),
+                                        dismissButton: .default(Text("Okay"))
+                                    )
+                                case "invalidAmount":
+                                    return Alert(
+                                        title: Text("Input Invalid"),
+                                        message: Text("Your initial balance can only be a numerical value"),
+                                        dismissButton: .default(Text("Okay"))
+                                    )
+                                default:
+                                    return Alert(
+                                        title: Text("Unknown Error"),
+                                        message: Text("Try again later"),
+                                        dismissButton: .default(Text("Okay"))
+                                    )
                             }
                         }
-                    }, label: {
-                        Text("Save")
-                    }).disabled(name.isEmpty || amount.isEmpty)
-                    .alert(isPresented: $showAlert) {
-                        switch alertType {
-                            case "existingAcct":
-                                return Alert(
-                                    title: Text("Error"),
-                                    message: Text("This account already exists"),
-                                    dismissButton: .default(Text("Okay"))
-                                )
-                            case "invalidAmount":
-                                return Alert(
-                                    title: Text("Input Invalid"),
-                                    message: Text("Your initial balance can only be a numerical value"),
-                                    dismissButton: .default(Text("Okay"))
-                                )
-                            default:
-                                return Alert(
-                                    title: Text("Unknown Error"),
-                                    message: Text("Try again later"),
-                                    dismissButton: .default(Text("Okay"))
-                                )
-                        }
+                        .padding(.horizontal, geom.size.width*0.1).font(.custom("button", size: 17))
+                        Button(action: {
+                                self.presentationMode.wrappedValue.dismiss()
+                        }, label: {
+                            Text("Cancel")
+                        }).foregroundColor(.secondary).padding(.horizontal, geom.size.width*0.1).font(.custom("button", size: 17))
                     }
-                    .padding(.horizontal, geom.size.width*0.1).font(.custom("button", size: 17))
-                    Button(action: {
-                            self.presentationMode.wrappedValue.dismiss()
-                    }, label: {
-                        Text("Cancel")
-                    }).foregroundColor(.secondary).padding(.horizontal, geom.size.width*0.1).font(.custom("button", size: 17))
-                }
-            }.padding(geom.size.width*0.1)
+                }.padding(.horizontal, geom.size.width*0.1)
+            }
         }
     }
 }
@@ -93,49 +95,51 @@ struct CategoryForm: View {
     
     var body: some View {
         GeometryReader { geom in
-            VStack (spacing: 30) {
-                HStack {
-                    Text("New category").font(.largeTitle).fontWeight(.bold)
-                    Spacer()
-                }.padding(.bottom, geom.size.height*0.02)
-                HStack {
-                    TextField("Enter Category", text: $name).padding().border(Color.gray).cornerRadius(8.0)
-                    Spacer()
-                }
-                Spacer()
-                HStack {
-                    Button(action: {
-                        self.ref.child("addCategory/categories/").getData { (error, snapshot) in
-                            var array:[String] = []
-                            for child in snapshot.children {
-                                let snap = child as! DataSnapshot
-                                let val = snap.value as! String
-                                if (val == name) {
-                                    showAlert = true
-                                }
-                                array.append(val)
-                            }
-                            array.append(name)
-                            self.ref.child("addCategory").setValue(["categories": array])
-                        }
-                    }, label: {
-                        Text("Save")
-                    }).disabled(name.isEmpty)
-                    .alert(isPresented: $showAlert) {
-                        Alert(
-                            title: Text("Error"),
-                            message: Text("This category already exists"),
-                            dismissButton: .default(Text("Okay"))
-                        )
+            ScrollView {
+                VStack (spacing: 30) {
+                    HStack {
+                        Text("New category").font(.largeTitle).fontWeight(.bold)
+                        Spacer()
+                    }.padding(.bottom, geom.size.height*0.02)
+                    HStack {
+                        TextField("Enter Category", text: $name).padding().border(Color.gray).cornerRadius(8.0)
+                        Spacer()
                     }
-                    .padding(.horizontal, geom.size.width*0.1).font(.custom("button", size: 17))
-                    Button(action: {
-                            self.presentationMode.wrappedValue.dismiss()
-                    }, label: {
-                        Text("Cancel")
-                    }).foregroundColor(.secondary).padding(.horizontal, geom.size.width*0.1).font(.custom("button", size: 17))
-                }
-            }.padding(geom.size.width*0.1)
+                    Spacer()
+                    HStack {
+                        Button(action: {
+                            self.ref.child("addCategory/categories/").getData { (error, snapshot) in
+                                var array:[String] = []
+                                for child in snapshot.children {
+                                    let snap = child as! DataSnapshot
+                                    let val = snap.value as! String
+                                    if (val == name) {
+                                        showAlert = true
+                                    }
+                                    array.append(val)
+                                }
+                                array.append(name)
+                                self.ref.child("addCategory").setValue(["categories": array])
+                            }
+                        }, label: {
+                            Text("Save")
+                        }).disabled(name.isEmpty)
+                        .alert(isPresented: $showAlert) {
+                            Alert(
+                                title: Text("Error"),
+                                message: Text("This category already exists"),
+                                dismissButton: .default(Text("Okay"))
+                            )
+                        }
+                        .padding(.horizontal, geom.size.width*0.1).font(.custom("button", size: 17))
+                        Button(action: {
+                                self.presentationMode.wrappedValue.dismiss()
+                        }, label: {
+                            Text("Cancel")
+                        }).foregroundColor(.secondary).padding(.horizontal, geom.size.width*0.1).font(.custom("button", size: 17))
+                    }
+                }.padding(geom.size.width*0.1)
+            }
         }
     }
 }
