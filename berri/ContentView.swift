798925fc2c62a499ee004ef1ac63e93a5e941d24
iheppe <iheppe@uw.edu>
2021-06-01 17:06:18 -0700

    add in money form file

--- berri/ContentView.swift
+++ berri/ContentView.swift
@@ -14,33 +14,6 @@ struct ContentView: View {
     }
 }
 
-struct AddMoney: View {
-    @State var selector = 1
-    var body: some View {
-        NavigationView {
-            VStack {
-                Text("Choose category: ")
-                Picker(selection: $selector, label: Text("Category"), content: {
-                    Text("Expense").tag(1)
-                    Text("Income").tag(2)
-                    Text("Transfer").tag(3)
-                })
-                NavigationLink(
-                    destination: ConfirmAccount(),
-                    label: {
-                        Text("Next")
-                    })
-            }
-        }
-    }
-}
-
-struct ConfirmAccount: View {
-    var body: some View {
-        Text("Money")
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
